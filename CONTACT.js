f78c6bd03dfdc18af7e7725ac82d248591e7a430
Pedro Prieto <pedro.prieto003@gmail.com>
2019-09-23 16:47:09 -0400

    Finished, still needs media breakpoints and footer

---
+++ CONTACT.js
@@ -0,0 +1,12 @@
+let accordion = document.querySelectorAll(".accordion");
+console.log(accordion);
+
+accordion.forEach(function(value){
+    value.onclick = function(){
+        let content = this.nextElementSibling;
+        if(content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else{
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }})
